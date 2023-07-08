67326bbad5be5a0d25bcb4b95fcad5f333878a77
Luna Prey <lunaprey7@gmail.com>
2023-07-07 23:11:19 -0500

    Update register.php
    Moved nav bar code to its own header.php file

--- register.php
+++ register.php
@@ -63,38 +63,10 @@ $conn->close();
     </style>
 </head>
 <body>
-     <nav class="navbar navbar-dark">
-      <div class="d-flex align-items-center">
-        <a class="navbar-brand, glow mr-3" href="#">
-          <i class="fas fa-moon"></i>
-        </a>
-        <span class="navbar-text"><a href="#">MoonDance</a></span>
-      </div>
-      <?php
-        if (isset($_SESSION['userid'])) {
-            // Logout Button
-        ?>
-        <div class="ml-auto">
-            <span class="navbar-text mr-2"><a href="#"><?php echo $_SESSION['userid']; ?></a></span>
-            <a href="logout.php" class="">Logout</a>
-        </div>
-        <?php
-        } else {
-            // Show Login form and Register Button
-        ?>
-        <div class="ml-auto">
-            <form action="login.php" class="form-inline">
-                <input class="black-background" type="text" placeholder="Username" aria-label="Username">
-                <input class="black-background" type="password" placeholder="Password" aria-label="Password">
-                <button class="black-background" type="submit">Login</button>
-            </form>
-            <a href="register.php" class="ml-auto">Register</a>
-        </div>
-        <?php
-        }
-        ?>
-        </nav>
-
+    <header>
+        <?php include 'https://raw.githubusercontent.com/lunaprey/MoonDance/main/header.php'; ?>
+    </header>
+     
     <div id="container">
         <h2>User Registration</h2>
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
