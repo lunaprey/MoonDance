7aeb597cfdafc8e0cae512d1618ab287b52cffdc
Luna Prey <lunaprey7@gmail.com>
2023-07-08 06:53:44 -0500

    Update new-topic.php

--- new-topic.php
+++ new-topic.php
@@ -16,9 +16,19 @@
   <div id="container">
     New Topic
     <div class="forum-category">
-      <input class="black-background" type="text" name="subject"><br/>
-      <textbox></textbox><br/>
-      <input value="Create Topic" type="submit">
+      <form action="new-topic.php" method="POST">
+        <div class="form-group">
+          <label for="subject">Subject:</label>
+          <input type="text" id="subject" name="subject" placeholder="Enter the subject">
+        </div>
+        <div class="form-group">
+          <label for="message">Message:</label>
+          <textarea id="message" name="message" placeholder="Enter your message"></textarea>
+        </div>
+        <div class="form-group">
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
   </div>
 
