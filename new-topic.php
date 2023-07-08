11401c3bcacaf3d29d175cd82832f0e04f4b07e2
Luna Prey <lunaprey7@gmail.com>
2023-07-08 06:37:27 -0500

    Create new-topic.php

---
+++ new-topic.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>New Topic</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <style>
+    <?php include 'https://raw.githubusercontent.com/lunaprey/MoonDance/main/style.css'; ?>
+  </style>
+</head>
+<body>
+  <header>
+    <?php include 'https://raw.githubusercontent.com/lunaprey/MoonDance/main/header.php'; ?>
+  </header>
+  
+  <div id="container">
+    New Topic
+    <div class="forum-category">
+      <h2><a href="#">Welcome</a></h2>
+      <p>MoonDance</p>
+    </div>
+  </div>
+
+  
+  <footer>
+    <p>An opensource project. See <a href="https://github.com/lunaprey/MoonDance">https://github.com/lunaprey/MoonDance</a> to contribute.</p>
+  </footer>
+  
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
