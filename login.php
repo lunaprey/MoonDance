02877f7dfec43ef2f5ec128ffd47bd88e11355c6
Luna Prey <lunaprey7@gmail.com>
2023-07-08 00:57:11 -0500

    Update login.php
    Getting login.php to work

--- login.php
+++ login.php
@@ -1,9 +1,46 @@
 <?php
 session_start();
-$_SESSION['userid'] = 1;
-$_SESSION['username'] = 'username';
+// Creds moved outside of this file for security, but code should still work
 
-// Redirect back to index.php
-header("Location: index.php");
-exit();
+// Establishing a database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Handling form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = isset($_POST["username"]) ? $_POST["username"] : "";
+    $password = isset($_POST["password"]) ? $_POST["password"] : "";
+
+    // Validate and sanitize the input
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+    $password = filter_var($password, FILTER_SANITIZE_STRING);
+
+    // Prepare the query using prepared statements to prevent SQL injection
+    $check_query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $stmt = $conn->prepare($check_query);
+    $stmt->bind_param("ss", $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        // User exists, redirect to index.php or perform login logic
+        $_SESSION['userid'] = $user['id'];
+        $_SESSION['username'] = $user['username'];
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Wrong! Press back and try again.";
+        exit();
+    }
+
+    // Close the statement
+    $stmt->close();
+}
+
+// Close the database connection
+$conn->close();
 ?>
